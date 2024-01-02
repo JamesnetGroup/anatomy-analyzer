4f91daefe825278e7279771756038b2e02c0f682
JAEWUNG LEE <james@jamesnet.dev>
2024-01-03 04:38:21 +0900

    Updates

--- src/Anatomy/App.cs
+++ src/Anatomy/App.cs
@@ -2,8 +2,10 @@
 using Anatomy.Forms.UI.Views;
 using Anatomy.Support.Local.Helpers;
 using Anatomy.Support.Local.Models;
+using Anatomy.Support.UI.Units;
 using DemoApp.Support.UI.Units;
 using Jamesnet.Wpf.Controls;
+using NavigationBar;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,10 +39,19 @@ namespace Anatomy
 
         protected virtual void RegisterControls(AnatomyItemCollection items)
         {
+            var bar =new MagicBar();
+            bar.Items.Add(new ListBoxItem { Content ="Microsoft", Tag = IconType.Microsoft });
+            bar.Items.Add(new ListBoxItem { Content = "Apple", Tag = IconType.Apple });
+            bar.Items.Add(new ListBoxItem { Content = "Google", Tag = IconType.Google});
+            bar.Items.Add(new ListBoxItem { Content = "Facebook", Tag = IconType.Facebook });
+            bar.Items.Add(new ListBoxItem { Content = "Instagram", Tag = IconType.Instagram });
+
+
             items.Add(new Button().InitSampleData(Theme.Black));
             items.Add(new Slider().InitSampleData(Theme.Black));
             items.Add(new TabControl().InitSampleData(Theme.Black));
             items.Add(new ListBox().InitSampleData(Theme.Black));
+            items.Add(new RiotSlider().InitSampleData(Theme.Black));
             items.Add(new ThemeSwitch.UI.Units.ThemeSwitch().InitSampleData(Theme.Black));
             items.Add(new FilledButton().InitSampleData(Theme.Black));
             items.Add(new BorderedButton().InitSampleData(Theme.Black));
@@ -49,6 +60,7 @@ namespace Anatomy
             items.Add(new ToggleSwitch().InitSampleData(Theme.Black));
             items.Add(new UnderlineMenuBox().InitSampleData(Theme.Black));
             items.Add(new PlayButton());
+            items.Add(bar);
         }
     }
 }
